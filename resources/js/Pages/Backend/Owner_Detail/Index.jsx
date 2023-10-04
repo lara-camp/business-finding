3f26ccf14329bc3f5cf1c07da97f432a34ed1617
Thuyatun <thuratundev@gmail.com>
2023-10-04 23:07:09 +0630

    merge main branch

--- resources/js/Pages/Backend/Owner_Detail/Index.jsx
+++ resources/js/Pages/Backend/Owner_Detail/Index.jsx
@@ -3,6 +3,7 @@ import Table from "../../Test/Table";
 import BackendLayout from "@/Layouts/BackendLayout";
 
 const Index = ({ ownerdetails }) => {
+<<<<<<< Updated upstream
     const columns = [
         "id",
         "user_id",
@@ -14,6 +15,11 @@ const Index = ({ ownerdetails }) => {
     ];
     const routeName = "owner";
     const title = "owner";
+=======
+    const columns = ['id', 'user_id', 'address', 'company', 'frontend_img', 'backend_img', 'action'];
+    const routeName = "owner"
+    const title = "ownerdetail"
+>>>>>>> Stashed changes
     return (
         <div>
             <Table
@@ -30,5 +36,10 @@ const Index = ({ ownerdetails }) => {
     );
 };
 
+<<<<<<< Updated upstream
 Index.layout = (page) => <BackendLayout children={page} title="owner" />;
 export default Index;
+=======
+Index.layout = page => <BackendLayout children={page} title="ownerdetail" />
+export default Index
+>>>>>>> Stashed changes
