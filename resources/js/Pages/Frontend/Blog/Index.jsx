dbdde63b2297ee46b117e3bfd37034a604005d26
Phyo Phyo Kyaw <phyophyokyawppk1@gmail.com>
2023-10-12 14:44:10 +0630

    update

--- resources/js/Pages/Frontend/Blog/Index.jsx
+++ resources/js/Pages/Frontend/Blog/Index.jsx
@@ -14,16 +14,16 @@ const Index = (news) => {
             <a href={`/blog/details?tags='News'`}>
                 <h1 class="mx-3 text-5xl text-dark mt-2">News</h1>
             </a>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
                 {news['news']['data'].map((item, index) => (
-                    <div key={index} className="col-span-1 relative">
+                    <div key={index} className="relative col-span-1 px-2 duration-300 ease-in-out delay-150 shadow-lg tran sition mt-3mx-5 bg-amber-50 hover:-translate-y-1 hover:scale-110">
                         <div class="py-10 sm:py-8 mb-3 mx-5">
                             <div class="mx-auto max-w-2xl">
-                                <article class="items-start justify-between">
+                                <article class="items-start justify-between ">
                                     <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                                         {item.body.length > 20 ? `${item.body.substring(0, 50)}...` : item.body}
                                     </h3>
-                                    <div class="relative w-full mt-2">
+                                    <div class="relative w-full mt-2 shadow-lg ">
                                         <img src={item.url} alt="image" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover" />
                                         <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
                                     </div>
